25275407e3aeee42426b42a90b2921663260e4b7
eriklinnarson <erik.linnarson@outlook.com>
2022-09-30 21:54:36 +0200

    Search screen looks better with WoW logo.

--- WotlkArmory/WotlkArmory/Views/SearchView.swift
+++ WotlkArmory/WotlkArmory/Views/SearchView.swift
@@ -12,17 +12,28 @@ struct SearchView: View {
     @State var searchString: String = ""
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                HStack {
-                    TextField("Character name", text: $searchString)
-                    NavigationLink(destination: SearchResultView(searchString: searchString)) {
-                        Text("Search")
+        GeometryReader { geometry in
+            NavigationView {
+                VStack {
+                    Image("wotlk_classic_logo")
+                        .resizable()
+                        .scaledToFit()
+                    HStack {
+                        Spacer()
+                        TextField("Character name", text: $searchString)
+                            .fixedSize()
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        NavigationLink(destination: SearchResultView(searchString: searchString)) {
+                            Image(systemName: "magnifyingglass.circle.fill")
+                        }
+                        Spacer()
                     }
+                    .padding()
+                    .font(.title)
                 }
+                .navigationTitle("Search")
+                .navigationBarHidden(true)
             }
-            .navigationTitle("Search")
-            .navigationBarHidden(true)
         }
     }
 }
